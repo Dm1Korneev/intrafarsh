5621700741d1092612f91d48afaaf81675f5407f
St1llman <dm1korneev@gmail.com>
2020-02-08 13:43:40 +0300

    add google map on route page

--- src/Components/RoutePage/index.js
+++ src/Components/RoutePage/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { GoogleApiWrapper, Map, Marker } from 'google-maps-react';
+import { GOOGLE_MAP_API_KEY } from 'Constants';
 
-import { Container } from './RoutePage.styled';
+const containerStyle = {
+  width: '100%',
+  height: '100%',
+};
 
-const RoutePage = () => (
-  <Container>
-      Route
-  </Container>
+const RoutePage = (props) => (
+  <Map
+    zoom={16}
+    containerStyle={containerStyle}
+    initialCenter={{
+      lat: 55.7087173,
+      lng: 37.7277525,
+    }}
+    streetViewControl={false}
+    mapTypeControl={false}
+    {...props}
+  >
+    <Marker
+      name="Congress centre Technopolis Moscow"
+      position={{
+        lat: 55.7083437,
+        lng: 37.7245968,
+      }}
+    />
+  </Map>
 );
 
-export default RoutePage;
+export default GoogleApiWrapper({
+  apiKey: GOOGLE_MAP_API_KEY,
+})(RoutePage);
