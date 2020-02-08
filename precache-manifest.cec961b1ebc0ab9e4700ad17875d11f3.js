e3164de1b40334cfb9f6268b11da3b964aab37c9
Dm1Korneev <runner@fv-az33.x3bb2ck5feperbm4dgtm3rqj4d.cx.internal.cloudapp.net>
2020-02-08 20:38:43 +0000

    Updates

--- precache-manifest.cec961b1ebc0ab9e4700ad17875d11f3.js
+++ precache-manifest.cec961b1ebc0ab9e4700ad17875d11f3.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3b66766477dd166c5278fd88ea0d8259",
+    "revision": "472ba9f8281f2e48cb2fa7957b4e2620",
     "url": "/index.html"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b143c57e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6abf50a7bb9135d700b2",
-    "url": "/static/js/main.60fbc71c.chunk.js"
+    "revision": "79583a7d88df90fe105e",
+    "url": "/static/js/main.16ea48c8.chunk.js"
   },
   {
     "revision": "73baec0fb9bb57d739be",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.69cf856467f89668a05fde9766aecf7f.js"
+  "/precache-manifest.cec961b1ebc0ab9e4700ad17875d11f3.js"
 );
 
 self.addEventListener('message', (event) => {
